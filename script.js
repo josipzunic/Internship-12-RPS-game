fa082001b0e1ca0304d6ed4cb7c014c54d9f90d6
josipzunic <jzunic123@gmail.com>
2026-02-08 17:24:04 +0100

    Implement game flow for a single mock game

--- script.js
+++ script.js
@@ -76,7 +76,8 @@ async function getRequest(gameId) {
         const data = await response.json();
 
         const img = document.querySelector(".computer-move img");
-        img.src = data.data.playerMoveImg;
+        img.src = moveAndImageSrc[data.data.computerMove];
+        return [data.data.computerMove, data.data.playerMove];
     } catch {
         throw new Error("something went wrong");
     }
@@ -86,6 +87,13 @@ async function putRequest(gameId, playerMove, imageSrc) {
     try {
         const response = await fetch(
             `https://api.restful-api.dev/objects/${gameId}`,
+        );
+        const data = await response.json();
+        const computerMove = data.data.computerMove;
+        const gameSetId = data.data.gameSetId;
+
+        const res = await fetch(
+            `https://api.restful-api.dev/objects/${gameId}`,
             {
                 method: "PUT",
                 headers: {
@@ -94,6 +102,8 @@ async function putRequest(gameId, playerMove, imageSrc) {
                 body: JSON.stringify({
                     data: {
                         playerMove: playerMove,
+                        computerMove,
+                        gameSetId,
                         playerMoveImg: imageSrc,
                     },
                 }),
@@ -102,6 +112,18 @@ async function putRequest(gameId, playerMove, imageSrc) {
     } catch {
         throw new Error("something went wrong");
     }
+}
+
+function determineWinner(computer, player) {
+    const winningCombinations = {
+        rock: "scissors",
+        paper: "rock",
+        scissors: "paper",
+    };
+
+    if (player === computer) return "tie";
+    if (winningCombinations[player] === computer) return "player";
+    return "computer";
 }
 
 const gameDisplayButtons = document.querySelectorAll(".game-display button");
@@ -117,9 +139,24 @@ const computerMove = document.querySelector(".computer-move");
 const [rock, paper, scissors] = document.querySelectorAll(
     ".rock-paper-scissors-buttons img",
 );
+const winnerContainer = document.querySelector(".winner");
+const winnerContainerSpan = document.querySelector(".winner span");
 let gameIdArray = [];
+let gameNumber = 1;
+const movesArray = [
+    { el: rock, value: "rock" },
+    { el: paper, value: "paper" },
+    { el: scissors, value: "scissors" },
+];
+
+const moveAndImageSrc = {
+    rock: "/assets/images/fist.png",
+    paper: "/assets/images/hand-paper.png",
+    scissors: "/assets/images/scissors.png",
+};
 
 newGameButton.addEventListener("click", async () => {
+    gameIdArray = [];
     hideNewGameDisplay();
     showLoader();
     await generateGames(gameIdArray);
@@ -136,33 +173,34 @@ newGameButton.addEventListener("click", async () => {
 });
 
 returnButton.addEventListener("click", () => {
+    gameNumber = 1;
     hideGameDisplay();
     displayNewGameDisplay();
 });
 
 startGameButton.addEventListener("click", () => {
+    gameNumber = 1;
     hideGameDisplay();
     playGame();
 });
 
-let gameNumber = 1;
-const rockValue = "rock";
-const paperValue = "paper";
-const scissorsValue = "scissors";
+movesArray.forEach(({ el, value }) => {
+    el.addEventListener("click", async () => {
+        ["rock", "paper", "scissors"]
+            .filter((move) => move !== value)
+            .forEach((move) => {
+                const moveEl = gameplayDisplay.querySelector(`.${move}`);
+                moveEl.style.display = "none";
+            });
 
-const moveAndImageSrc = {
-    rock: "assets/images/fist.png",
-    paper: "assets/images/hand-paper.png",
-    scissors: "assets/images/scissors.png",
-};
+        const gameId = gameIdArray[gameNumber - 1];
+        await putRequest(gameId, value, moveAndImageSrc[value]);
 
-rock.addEventListener("click", async () => {
-    const gameId = gameIdArray[gameNumber - 1];
-    await putRequest(
-        gameId,
-        rockValue,
-        moveAndImageSrc.rock,
-    );
-    await getRequest(gameId);
-    gameNumber++;
+        gameNumber++;
+
+        const [computerChoice, playerChoice] = await getRequest(gameId);
+        const winner = determineWinner(computerChoice, playerChoice);
+        winnerContainerSpan.innerHTML = winner;
+        winnerContainer.style.display = "block";
+    });
 });
