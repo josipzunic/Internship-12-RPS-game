73652d4c2de5fae0c08449000f46b1cfbee63fc1
josipzunic <jzunic123@gmail.com>
2026-02-08 21:19:44 +0100

    Add review option

--- script.js
+++ script.js
@@ -80,7 +80,7 @@ function hideGame() {
 
 function resetComputerImage() {
     const compImg = computerMove.querySelector("img");
-    compImg.src = "/assets/images/question-sign.png";
+    compImg.src = "/assets/images/6fd.jpg";
 }
 
 function hideWinner() {
@@ -151,7 +151,7 @@ const beginGameDisplay = document.querySelector(".begin-game");
 const returnButton = gameDisplayButtons[0];
 const startGameButton = gameDisplayButtons[1];
 const gameDisplay = document.querySelector(".game-display");
-const newGameButton = document.querySelector(".begin-game button");
+const newGameButton = document.querySelectorAll(".begin-game button")[0];
 const loader = document.querySelector(".loader");
 const gameplayDisplay = document.querySelector(".rock-paper-scissors-buttons");
 const versus = document.querySelector(".versus");
@@ -163,6 +163,12 @@ const winnerContainer = document.querySelector(".winner-container");
 const winnerContainerSpan = document.querySelector(".winner-container span");
 const nextGameButton = document.querySelector(".winner-container button");
 const game = document.querySelector(".rock-paper-scissors-game");
+const reviewButton = document.querySelectorAll(".begin-game button")[1];
+const returnFromReviewButton = document.querySelector(
+    ".review-container button",
+);
+const reviewContainer = document.querySelector(".review-container");
+const reviewList = document.querySelector(".review-list");
 let gameIdArray = [];
 let gameNumber = 1;
 const movesArray = [
@@ -172,9 +178,9 @@ const movesArray = [
 ];
 
 const moveAndImageSrc = {
-    rock: "/assets/images/fist.png",
-    paper: "/assets/images/hand-paper.png",
-    scissors: "/assets/images/scissors.png",
+    rock: "/assets/images/rockrock.png",
+    paper: "/assets/images/Toilet-Paper.png",
+    scissors: "/assets/images/scissorsscissors.png",
 };
 
 newGameButton.addEventListener("click", async () => {
@@ -184,14 +190,6 @@ newGameButton.addEventListener("click", async () => {
     await generateGames(gameIdArray);
     hideLoader();
     displayGameDisplay();
-
-    const query = gameIdArray.map((id) => `id=${id}`).join("&");
-
-    const response = await fetch(
-        `https://api.restful-api.dev/objects?${query}`,
-    );
-    const data = await response.json();
-    console.log(data);
 });
 
 returnButton.addEventListener("click", () => {
@@ -222,7 +220,7 @@ movesArray.forEach(({ el, value }) => {
 
         const [computerChoice, playerChoice] = await getRequest(gameId);
         const winner = determineWinner(computerChoice, playerChoice);
-        
+
         winnerContainer.style.display = "flex";
         winnerContainerSpan.innerHTML += " " + winner;
     });
@@ -238,11 +236,80 @@ nextGameButton.addEventListener("click", () => {
         hideWinner();
         return;
     }
-    
+
     hideWinner();
     ["rock", "paper", "scissors"].forEach((move) => {
         const moveEl = gameplayDisplay.querySelector(`.${move}`);
         if (moveEl) moveEl.style.display = "block";
     });
     resetComputerImage();
+});
+
+reviewButton.addEventListener("click", async () => {
+    if (!gameIdArray.length) {
+        alert("No games to display");
+        return;
+    }
+
+    reviewList.innerHTML = "";
+
+    hideGameDisplay();
+    hideNewGameDisplay();
+    reviewList.style.display = "flex";
+    returnFromReviewButton.style.display = "block"; 
+
+    try {
+        const query = gameIdArray.map((id) => `id=${id}`).join("&");
+
+        const response = await fetch(
+            `https://api.restful-api.dev/objects?${query}`,
+        );
+        const data = await response.json();
+
+        let winner,
+            playerWinCount = 0,
+            computerWinCount = 0;
+
+        data.forEach((game, index) => {
+            winner = determineWinner(
+                game.data.computerMove,
+                game.data.playerMove,
+            );
+            if (winner === "player") playerWinCount++;
+            else if (winner === "computer") computerWinCount++;
+
+            const reviewText = `ROUND #${index + 1} -> player: ${game.data.playerMove} - computer: ${game.data.computerMove} - winner: ${winner}`;
+
+            const reviewItem = document.createElement("div");
+            reviewItem.innerText = reviewText;
+
+            reviewItem.style.border = "5px solid #192bc2";
+            reviewItem.style.fontSize = "2rem";
+            reviewItem.style.width = "600px";
+
+            reviewList.appendChild(reviewItem);
+        });
+
+        if (playerWinCount > computerWinCount) winner = "player";
+        else if (playerWinCount < computerWinCount) winner = "computer";
+        else winner = "no one";
+
+        const finalScore = `Winner is ${winner}: ${playerWinCount}:${computerWinCount}`;
+        const reviewItem = document.createElement("div");
+        reviewItem.innerText = finalScore;
+
+        reviewItem.style.border = "5px solid #192bc2";
+        reviewItem.style.fontSize = "2rem";
+        reviewItem.style.width = "600px";
+
+        reviewList.appendChild(reviewItem);
+    } catch (error) {
+        throw new Error("something went wrong", error);
+    }
+});
+
+returnFromReviewButton.addEventListener("click", () => {
+    reviewContainer.style.display = "none";
+    returnFromReviewButton.style.display = "none";
+    displayGameDisplay();
 });
